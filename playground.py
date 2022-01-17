878ce84c441cf54966a4bfa571e560566faf9b7c
prateek <prateek.patel@vantage-power.com>
2022-01-17 21:51:38 +0000

    refactored code

--- playground.py
+++ playground.py
@@ -20,6 +20,8 @@ session = Session(default_bucket=bucket)
 data_dir = session.upload_data('transfusion.data', bucket=bucket, key_prefix='data')
 training_dir = "/".join(i for i in data_dir.split("/")[:-1])
 
+
+# Configure estimator
 job_name = 'training'
 output_path = f"s3://{bucket}/model/artifacts"
 hyperparameters = {'alpha': 0.1}
@@ -27,33 +29,34 @@ hyperparameters = {'alpha': 0.1}
 metrics = [{'Name': 'accuracy', 'Regex': "accuracy=([0-9\\.]+);"}]
 
 estimator = Estimator(image_uri=image_uri,
-                     role=role,
-                    instance_type='ml.m5.large',
+                      role=role,
+                      instance_type='ml.m5.large',
                       instance_count=1,
-                   output_path=output_path,
-                   use_spot_instances=True,
-                   base_job_name=job_name,
-                   metric_definitions=metrics,
-                   max_wait=100000,
-                   hyperparameters=hyperparameters,
-                   )
+                      output_path=output_path,
+                      use_spot_instances=True,
+                      base_job_name=job_name,
+                      metric_definitions=metrics,
+                      max_wait=100000,
+                      hyperparameters=hyperparameters,
+                     )
 
-
-ts = datetime.now().strftime("%Y%m%dT%Hh%Mm")
-
+# Setup experiment and trial
 try:
     experiment = Experiment.create(experiment_name='linear-models', 
                               description='Are linear models > 90% accurate')
 except:
     print("Experiment exists")
 
+
+ts = datetime.now().strftime("%Y%m%dT%Hh%Mm")
 trial = Trial.create(experiment_name=experiment.experiment_name,
                     trial_name=f"vanilla-SGD-{ts}")
 
 with Tracker.create(display_name='metadata') as tracker:
     tracker.log_parameters({'bucket':bucket, 'metrics':'accuracy'})
     
-    
+trial.add_trial_component(tracker.trial_component)
+
 experiment_config = {"ExperimentName": experiment.experiment_name,
                      "TrialName": trial.trial_name,
                      "TrialComponentDisplayName": job_name}
