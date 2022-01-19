56d74f1291fee6d8f3fc7b3b8443aef1c97c87b5
prateek <prateek.patel@vantage-power.com>
2022-01-19 14:57:43 +0000

    exploratory data analysis and report

---
+++ eda.py
@@ -0,0 +1,95 @@
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier, LogisticRegressionCV
+from sklearn.inspection import PartialDependenceDisplay
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.preprocessing import *
+from sklearn.pipeline import make_pipeline
+
+load_dotenv('local_credentials.env')
+
+
+data = 'transfusion.data'
+df = pd.read_csv(data)
+
+df.head()
+
+df.info()
+
+target = 'whether he/she donated blood in March 2007'
+features = df.columns.difference([target])
+
+ax = sns.pairplot(df.rename({target:'target'}, axis=1), hue='target', height=1.5)
+ax.savefig('resources/data-pairplot.png')
+
+# Model variable relationship to target using no-parametric estimate 
+X, y = df[features], df[target]
+estimator = RandomForestClassifier()
+
+estimator.fit(X,y)
+
+fig, ax = plt.subplots(figsize=(9,3))
+pdp = PartialDependenceDisplay.from_estimator(estimator, X, features, target=target,kind='both', n_cols=len(features), ax=ax,
+                                        ice_lines_kw={'alpha':0.2, 'linewidth':0.5},
+                                        pd_line_kw={'color':'k', 'linestyle':'-'},
+                                        subsample=200,
+                                       grid_resolution=20, n_jobs=-1)
+
+pdp.figure_.savefig('resources/feature-relationship.png')
+
+
+# Model PCA versions of variables
+pca = PCA(whiten=True, n_components=3) # Drop the redundant component
+Xt = pca.fit_transform(X)
+
+dft = pd.DataFrame(np.c_[Xt,y]).rename({Xt.shape[1]:'target'}, axis=1)
+ax = sns.pairplot(dft, hue='target', height=1.5)
+
+ax.savefig('resources/pca_features.png')
+
+estimator = make_pipeline(pca, RandomForestClassifier())
+estimator.fit(X,y)
+
+fig, ax = plt.subplots(figsize=(9,3))
+pdp = PartialDependenceDisplay.from_estimator(estimator, X, features, target=target,kind='both', n_cols=len(features), ax=ax,
+                                        ice_lines_kw={'alpha':0.2, 'linewidth':0.5},
+                                        pd_line_kw={'color':'k', 'linestyle':'-'},
+                                        subsample=200,
+                                       grid_resolution=20, n_jobs=-1)
+
+pdp.figure_.savefig('resources/pca-transformed-relationship.png')
+
+estimator = RandomForestClassifier()
+estimator.fit(Xt,y)
+fig, ax = plt.subplots(figsize=(9,3))
+pdp = PartialDependenceDisplay.from_estimator(estimator, Xt, range(Xt.shape[1]), target=target,kind='both', n_cols=Xt.shape[1], ax=ax,
+                                        ice_lines_kw={'alpha':0.2, 'linewidth':0.5},
+                                        pd_line_kw={'color':'k', 'linestyle':'-'},
+                                        subsample=200,
+                                       grid_resolution=20, n_jobs=-1)
+
+pdp.figure_.savefig('resources/decomposed-variables-relationship.png')
+
+# Modelling features as splines and using logistic model
+estimator = make_pipeline(SplineTransformer(n_knots=10,knots='quantile', extrapolation='linear'), 
+                          LogisticRegressionCV(Cs=np.logspace(1/10000,1000),
+                                               max_iter=10000,
+                                               n_jobs=-1, 
+                                               class_weight=None, 
+                                               scoring='neg_log_loss', 
+                                               cv=5))
+
+estimator.fit(Xt,y)
+fig, ax = plt.subplots(figsize=(9,3))
+pdp = PartialDependenceDisplay.from_estimator(estimator, Xt, range(Xt.shape[1]), target=target,kind='both', n_cols=Xt.shape[1], ax=ax,
+                                        ice_lines_kw={'alpha':0.2, 'linewidth':0.5},
+                                        pd_line_kw={'color':'k', 'linestyle':'-'},
+                                        subsample=200,
+                                       grid_resolution=20, n_jobs=-1)
+
+pdp.figure_.savefig('resources/linear-model-with-splines.png')
---
+++ report.md
@@ -0,0 +1,76 @@
+# Sagemaker Boilerplate report
+
+The current exercise is an attempt at simplifying use of Sagemaker for Data Science exploration using Blood Donation dataset. 
+In the process we can learn something about behaviour of data and better keep track of the whole modelling process.
+
+## Data
+
+columns : 
+* Recency (months)  
+* Frequency (times)
+* Monetary (c.c. blood)  
+* Time (months)
+* whether he/she donated blood in March 2007  
+
+**target : whether he/she donated blood in March 2007**
+
+N (instances) : 748
+Missing values : 0
+
+![Data distribution](./resources/data-pairplot.png)
+
+### Relationship of different variables with target
+```modelled with a RandomForestClassifier```
+![Feature relationship](./resources/feature-relationship.png)
+
+Observations:
+1. *Monetary (c.c. blood)* and *Frequency (times)* are essentially same variable and we can drop either from our analysis without any loss of information
+2. We can observe that variables have a non-linear relationship with *target* and so Linear models alone may underfit the data
+3. *Time (months)* appears to have an exponential decay like effect on target, with small peaks around 25-40 months. The time decay itself is not steep.
+4. *Recency (months)* also appears to have time decay effect and target variable decays much steeper then saturates
+5. *Frequency (times)* has a first order like increase followed by saturation of target variable
+6. There are a few outliers in the data and variables appear to have skewed distributions
+
+### Decomposing independent variables into their Principal components
+```Using PCA with 3 components and modelling with RandomClassifier```
+PCA will help remove the multicollinearity between *Monetary* and *Frequency* variables and it will also help if we decide to use a Logistic Regression model down the line.  
+
+**Relationship between variables after using PCA transformation**
+![PCA transformed relationship](./resources/pca-transformed-relationship.png)
+
+Observations:
+1. *Frequency (times)* becomes a completely redundant variable because another highly correlated feature exists in feature space
+2. *Monetary (c.c. blood)* has a much linear relationship than before
+3. Other variables show roughly same relationship
+4. This makes sense since the other 2 variables were largely un correlated to Monetary/Frequecy values
+
+**Relationship of decomposed variables with target**
+![Decomposed variables relationship](./resources/decomposed-variables-relationship.png)
+
+Observations:
+1. After PCA there are only 3 variables (one of the highly correlated variables disappears)
+2. Recency/Time behaviour is approximately represented by last 2 variables
+3. First PC shows exponential increase-saturation similar to Monetary/Frequency
+
+### Modelling assumptions
+
+1. There is no timeseries like structure in the data
+2. 3rd PC can be modelled as a spline with atleast 8-10 knots or a mixture of exponential and RBF to capture the bump in the middle and a decay overall
+3. 1st and 2nd PC can be modelled with exponential functions or splines with 4-5 knots
+4. Because the variables are not exactly uniformly distributed, knots can be placed at quantiles in data 
+5. The variables can be combined using a linear model with a log link to target
+6. To keep track of model performance *log-loss* can be used as scoring metric
+7. At this point we'll ignore the imbalance of classes in target variable
+
+**Using the assumptions above to roughly model the target (starting from decomposed variables)**
+![Linear model with splines](./resources/linear-model-with-splines.png)
+
+Observations:
+1. The rough selection of hyperparameters still yields a good `score (training log loss) = 0.45`
+2. The behaviour across different instances (blue line) is very regular and the splines are able to capture the relationships highlighted before (for decomposed variables)
+3. This model could be further tuned or supplemented with additional feature transformations to increase performance
+
+
+
+
+
